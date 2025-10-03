e323ae8d907a4084aec6e0d56d2ec9079d853f95
HappyN15 <118215544+HappyN15@users.noreply.github.com>
2025-10-03 12:11:56 -0600

    Ahora con TabView

--- NavigationStak/NavigationStak/ui/controladores/ui_navegacion.swift
+++ NavigationStak/NavigationStak/ui/controladores/ui_navegacion.swift
@@ -1,13 +1,45 @@
 import SwiftUI
 
+
+enum Pestañas{
+    case Inicio
+    case Configuracion
+    case Galeria
+    case Noticias
+}
+
 struct NavigationStak: View {
+    @State var pestaña_actual: Pestañas = .Inicio
     var body: some View {
         NavigationStack{
-            PantallaInicio()
+            
+            TabView(selection: $pestaña_actual){
+                Tab("Watch Now", systemImage: "play", value: .Inicio){
+                    NavigationStack{
+                        PantallaInicio()
+                    }
+                }
+                .badge("IDK")
+                Tab("Noticias", systemImage: "newspaper", value: .Noticias){
+                    NavigationStack{
+                        PantallaNoticias()
+                    }
+                }
+                .badge(noticias.count)
+                Tab("Galeria", systemImage: "photo.artframe", value: .Galeria){
+                    NavigationStack{
+                        PantallaGaleria()
+                    }
+                }
+                Tab("Configuracion", systemImage: "gear",value: .Configuracion){
+                    NavigationStack{
+                        PantallaConfig()
+                    }
+                }
+            }
         }
     }
 }
-
 #Preview {
     NavigationStak()
 }
